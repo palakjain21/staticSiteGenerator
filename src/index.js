4722192c499ca32a7f6d286dce4fcc5dfabcfa8b
palakjain21 <21palak2001@gmail.com>
2023-04-10 00:49:19 +0530

    added files

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const path = require("path");
 
 app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static("public"));
 
 let pic = "../public/shield.png";
