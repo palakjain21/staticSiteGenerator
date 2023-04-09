6d24cf498a28b5498945e253343c7485444e41b8
palakjain21 <21palak2001@gmail.com>
2023-04-10 00:46:53 +0530

    added files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const data = require("./data.json");
+const path = require("path");
 
+app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
